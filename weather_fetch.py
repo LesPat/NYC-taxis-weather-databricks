ca2255d0a2fd7876a5eff2226997dac45303a7bd
patryklesiak <pl72512@student.sgh.waw.pl>
2025-11-05 20:45:44 +0100

    first commit

---
+++ weather_fetch.py
@@ -0,0 +1,35 @@
+# Import Meteostat library and dependencies
+from datetime import datetime
+from meteostat import Point, Hourly
+import pandas as pd
+import boto3
+from io import StringIO
+
+# Set time period
+start = datetime(2024, 10, 1)
+end = datetime(2024, 12, 31)
+
+# Create Point for New York City, NY
+nyc = Point(40.7143, -74.006, 57)
+
+# Get daily data for Oct - Dec 2024
+data = Hourly(nyc, start, end)
+data = data.fetch()
+
+# Create CSV in memory
+csv_buffer = StringIO()
+data.to_csv(csv_buffer, index=False)
+
+s3 = boto3.client(
+    "s3"
+    #,aws_access_key_id="XXX",
+    #aws_secret_access_key="XXX",
+    #region_name="eu-north-1"
+)
+
+# Upload
+s3.put_object(
+    Bucket="nyc-taxis-traffic-analysis-raw",
+    Key="weather/nyc_weather_oct_dec_2024.csv",
+    Body=csv_buffer.getvalue()
+)
