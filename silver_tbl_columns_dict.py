d1f7161c539ac0b3cc779ee137fdc8b6f8c31c01
LesPat <lesiak-patryk@tlen.pl>
2025-12-11 19:39:05 +0000

    added file, updated silver table data ingestion
    >Python dictionary file was added to rename few columns in silver result table
    >Added partitioning by day for silver table
    >Cleaned gold_tbl_creation file

---
+++ silver_tbl_columns_dict.py
@@ -0,0 +1,11 @@
+silver_tbl_dict = {
+  "tpep_pickup_datetime" : "pickup_datetime",
+  "tpep_dropoff_datetime" : "dropoff_datetime",
+  "PUBorough" : "pickup_location",
+  "PUZone" : "pickup_zone",
+  "DOBorough" : "dropoff_location",
+  "DOZone" : "dropoff_zone",
+  "VendorID" : "vendor_id",
+  "RateCodeID" : "rate_code",
+  "Airport_fee" : "airport_fee"
+}
